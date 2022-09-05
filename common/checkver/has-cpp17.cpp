cee746de93626ce1b3adf07e7e50eb8cd3a11635
Egor Suvorov <egor_suvorov@mail.ru>
2022-09-05 16:23:18 +0300

    common/checkver: add has-cpp17

---
+++ common/checkver/has-cpp17.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "Hello World!\n";
+    std::vector vec{1, 2, 3, 4};  // Requires C++17.
+    std::cout << "1 == " << vec[0] << "\n";
+}
