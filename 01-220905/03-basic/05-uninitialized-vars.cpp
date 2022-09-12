f4a622b7f3b03f73604bf293cbf9398808d7370d
Egor Suvorov <egor_suvorov@mail.ru>
2022-09-12 12:41:10 +0300

    01-220905: add note way after classes

--- 01-220905/03-basic/05-uninitialized-vars.cpp
+++ 01-220905/03-basic/05-uninitialized-vars.cpp
@@ -9,5 +9,6 @@ int main() {
         int x;
         x = x * 0;  // reading from uninitialized variable is UB (undefined behavior): https://notes.algoprog.ru/cpp/additional.html#undefined-behavior
         std::cout << x << "\n";  // but it behaves ok with my compiler in this program
+        // Note: if not multiplied by `0`, it's constant on my Windows and something random on my Linux.
     }
 }
