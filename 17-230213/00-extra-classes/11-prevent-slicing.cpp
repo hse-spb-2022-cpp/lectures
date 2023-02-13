d1a81a146c52a92da26033658e83f6ab14e98920
Egor Suvorov <egor.suvorov@gmail.com>
2023-02-13 09:40:04 +0200

    17-230213: copy from next/13-211212: inheritance visibility, slicing

---
+++ 17-230213/00-extra-classes/11-prevent-slicing.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+struct Base {
+    int x = 10;
+    void foo() const {
+        std::cout << "x=" << x << "\n";
+    }
+
+    Base() {}
+    Base(const Base &) = delete;
+    Base(Base &&) = delete;
+    Base &operator=(const Base &) = delete;
+    Base &operator=(Base &&) = delete;
+};
+
+struct Derived : Base {
+    int y = 20;
+    void bar() const {
+        foo();
+        std::cout << "y=" << y << "\n";
+    }
+};
+
+void bar(Base b) {  // Slicing: we create a new object
+    std::cout << "bar(" << b.x << ")\n";
+    const Derived &d = static_cast<const Derived&>(b);  // derivedcast, UB
+    std::cout << ".y=" << d.y << "\n";  // UB
+    &d.y;  // UB
+}
+
+int main() {
+    {
+        Derived d;
+        bar(d);
+    }
+    {
+        Base b;
+        bar(b);
+    }
+}
